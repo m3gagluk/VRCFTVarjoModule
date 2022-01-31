53b27841001c71405c10ba0225621ca05da7bad6
m3gagluk <15657215+m3gagluk@users.noreply.github.com>
2022-01-31 08:45:40 +0300

    Reimplemented the Varjo software detection method without the external library

---
+++ VarjoCompanionInterface.cs
@@ -0,0 +1,102 @@
+﻿using MelonLoader;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace VRCFTVarjoModule
+{
+    // a dumb memory structure to copy all the data between the mod and the companion
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MemoryEye
+    {
+        public bool opened;
+        public double pupilSize;
+        public double x;
+        public double y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MemoryData
+    {
+        public bool shutdown;
+        public bool calibrated;
+        public MemoryEye leftEye;
+        public MemoryEye rightEye;
+        public MemoryEye combined;
+    }
+
+    public class VarjoCompanionInterface
+    {
+
+        private MemoryMappedFile MemMapFile;
+        private MemoryMappedViewAccessor ViewAccessor;
+        public MemoryData memoryGazeData;
+        private Process CompanionProcess;
+
+        public bool ConnectToPipe()
+        {
+            if (!VarjoAvailable())
+            {
+                MelonLogger.Msg("Varjo headset isn't detected");
+                return false;
+            }
+            var modDir = GetModuleDir();
+
+            CompanionProcess = new Process();
+            CompanionProcess.StartInfo.WorkingDirectory = modDir;
+            CompanionProcess.StartInfo.FileName = Path.Combine(modDir, "VarjoCompanion.exe");
+            CompanionProcess.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    MemMapFile = MemoryMappedFile.OpenExisting("VarjoEyeTracking");
+                    ViewAccessor = MemMapFile.CreateViewAccessor();
+                    return true;
+                }
+                catch (FileNotFoundException)
+                {
+                    MelonLogger.Warning("VarjoEyeTracking mapped file doesn't exist; the companion app probably isn't running");
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Error("Could not open the mapped file: " + ex);
+                    return false;
+                }
+                Thread.Sleep(500);
+            }
+
+            return false;
+        }
+
+        public void Update()
+        {
+            if (MemMapFile == null) return;
+            ViewAccessor.Read(0, out memoryGazeData);
+        }
+
+        public void Teardown()
+        {
+            if (MemMapFile == null) return;
+            memoryGazeData.shutdown = true; // tell the companion app to shut down gracefully but it doesn't work anyway
+            ViewAccessor.Write(0, ref memoryGazeData);
+            MemMapFile.Dispose();
+            CompanionProcess.Close();
+        }
+
+        private string GetModuleDir()
+        {
+            return MelonUtils.BaseDirectory + "\\Mods\\VRCFTLibs\\Varjo";
+        }
+
+        private bool VarjoAvailable()
+        {
+            // totally not now the official Varjo library works under the hood
+            return File.Exists("\\\\.\\pipe\\Varjo\\InfoService");
+        }
+    }
+}
---
+++ VarjoTrackingModule.cs
@@ -0,0 +1,82 @@
+﻿using MelonLoader;
+using System;
+using System.Threading;
+using VRCFaceTracking;
+using VRCFaceTracking.Params;
+using Vector2 = VRCFaceTracking.Params.Vector2;
+
+namespace VRCFTVarjoModule
+{
+   
+    // This class contains the overrides for any VRCFT Tracking Data struct functions
+    public static class TrackingData
+    {
+        // This function parses the external module's single-eye data into a VRCFT-Parseable format
+        public static void Update(this Eye data, MemoryEye external)
+        {
+            
+            data.Look = new Vector2((float) external.x, (float) external.y);
+            data.Openness = external.opened ? 1F : 0F;
+        }
+
+        // This function parses the external module's full-data data into multiple VRCFT-Parseable single-eye structs
+        public static void Update(this EyeTrackingData data, MemoryData external)
+        {
+            Update(data.Left, external.rightEye);
+            Update(data.Right, external.leftEye);
+            Update(data.Combined, external.combined);
+            Console.WriteLine(external.combined.opened);
+            Console.Write(" ");
+            Console.WriteLine(data.Combined.Openness);
+        }
+
+    }
+    
+    public class VarjoTrackingModule : ITrackingModule
+    {
+        private static readonly VarjoCompanionInterface tracker = new VarjoCompanionInterface();
+        private static CancellationTokenSource _cancellationToken;
+
+        // Synchronous module initialization. Take as much time as you need to initialize any external modules. This runs in the init-thread
+        public (bool eyeSuccess, bool lipSuccess) Initialize(bool eye, bool lip)
+        {
+            MelonLogger.Msg("Initializing Varjo module");
+            bool pipeConnected = tracker.ConnectToPipe();
+            return (pipeConnected, false);
+        }
+
+        // This will be run in the tracking thread. This is exposed so you can control when and if the tracking data is updated down to the lowest level.
+        public Action GetUpdateThreadFunc()
+        {
+            _cancellationToken = new CancellationTokenSource();
+            return () =>
+            {
+                while (!_cancellationToken.IsCancellationRequested)
+                {
+                    Update();
+                    Thread.Sleep(10);
+                }
+            };
+        }
+
+        // The update function needs to be defined separately in case the user is running with the --vrcft-nothread launch parameter
+        public void Update()
+        {
+            tracker.Update();
+            TrackingData.Update(UnifiedTrackingData.LatestEyeData, tracker.memoryGazeData);
+        }
+
+        // A chance to de-initialize everything. This runs synchronously inside main game thread. Do not touch any Unity objects here.
+        public void Teardown()
+        {
+            _cancellationToken.Cancel();
+            tracker.Teardown();
+            _cancellationToken.Dispose();
+            MelonLogger.Msg("Teardown");
+        }
+
+
+        public bool SupportsEye => true;
+        public bool SupportsLip => false;
+    }
+}
